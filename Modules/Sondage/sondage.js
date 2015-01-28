671ec393e98601782accca85b0d8e7a9ebf308df
louispepin <louis.pepin.ce@gmail.com>
2015-01-28 18:41:03 -0500

    Sondage++ et readme

--- Modules/Sondage/sondage.js
+++ Modules/Sondage/sondage.js
@@ -1,4 +1,73 @@
 angular.module('sondage', [])
-.controller('sondageCtrl', ['$scope', function($scope) {
-    document.title="Sondage";
-}]);
+    .controller('sondageCtrl', ['$scope', '$filter', function($scope, $filter) {
+        document.title="Sondage";
+
+        $scope.noProf = false;
+        $scope.noCharge = false;
+        $scope.noAux = false;
+
+        $scope.allProfs = [];
+        $scope.allCharges = [];
+        $scope.allAux = [];
+
+        $scope.vote = {
+            prof: '',
+            charge: '',
+            aux: ''
+        };
+
+        $scope.cours = [
+            {
+                id: 0,
+                name: ''
+            },
+            {
+                id: 1,
+                name: ''
+            },
+            {
+                id: 2,
+                name: ''
+            }
+        ];
+
+        $scope.index = 0;
+
+        $scope.actions = {
+            add: function () {
+                $scope.index++;
+                $scope.cours.push({
+                    id: $scope.index,
+                    name: ''
+                })
+            },
+
+            remove: function (c) {
+                var index;
+                for (var i=0; i<$scope.cours.length; i++) {
+                    if ($scope.cours[i].id == c.id) {
+                        index = i;
+                    }
+                }
+
+                $scope.cours.splice(index, 1);
+            },
+
+            submitClasses: function () {
+                alert(angular.toJson($scope.cours));
+            },
+
+            submitAll: function () {
+                if ($scope.noProf)
+                    $scope.vote.prof = null;
+
+                if ($scope.noCharge)
+                    $scope.vote.charge = null;
+
+                if ($scope.noAux)
+                    $scope.vote.aux = null;
+
+                alert(angular.toJson($scope.vote));
+            }
+        };
+    }]);
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-salut la gang
+Site du vote pour le Gala Meritas de l'Ecole Polytechnique de Montreal en Hiver 2015.
